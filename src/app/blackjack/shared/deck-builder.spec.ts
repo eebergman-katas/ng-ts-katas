99b418aa47bfefe563e12c2ee0f55a25f8298c17
Erin Bergman <ebergman@pillartechnology.com>
2017-05-18 10:02:17 -0400

    Create cards of each suit

--- src/app/blackjack/shared/deck-builder.spec.ts
+++ src/app/blackjack/shared/deck-builder.spec.ts
@@ -5,21 +5,34 @@ import { FaceValue } from 'app/blackjack/shared/face-value.enum';
 import { Suit } from 'app/blackjack/shared/suit.enum';
 
 fdescribe('Deck Builder Spec', () => {
+  let testDeckBuilder: DeckBuilder;
+  let testDeck: Deck;
 
-  describe('#cardGenerator', () => {
-    const firstCard: Card = new Card(Suit.Club, {
-      faceValue: FaceValue.Ace,
-      pointValue: 1
+  describe('Create cards of each suit', () => {
+    beforeEach(() => {
+      testDeckBuilder = new DeckBuilder();
+      testDeck = new Deck(); // this can be commented out?
     });
 
     it('should generate a card that is a club', () => {
-      const testDeckBuilder: DeckBuilder = new DeckBuilder();
-      let testDeck = new Deck();
       testDeck = testDeckBuilder.cardGenerator();
-
-      expect(testDeck.cards[0]).toEqual(firstCard);
+      expect(testDeck.cards[0].suit).toEqual(Suit.Club);
     });
 
+    it('should generate a card that is a diamond', () => {
+      testDeck = testDeckBuilder.cardGenerator();
+      expect(testDeck.cards[1].suit).toEqual(Suit.Diamond);
+    });
 
+    it('should generate a card that is a heart', () => {
+      testDeck = testDeckBuilder.cardGenerator();
+      expect(testDeck.cards[2].suit).toEqual(Suit.Heart);
+    });
+
+    it('should generate a card that is a spade', () => {
+      testDeck = testDeckBuilder.cardGenerator();
+      expect(testDeck.cards[3].suit).toEqual(Suit.Spade);
+    });
   });
+
 });
--- src/app/blackjack/shared/deck-builder.ts
+++ src/app/blackjack/shared/deck-builder.ts
@@ -7,11 +7,31 @@ export class DeckBuilder {
 
     public cardGenerator(): Deck {
         const deck: Deck = new Deck();
-        const firstCard = new Card(Suit.Club, {
-            faceValue: FaceValue.Ace,
-            pointValue: 1
-        });
-        deck.cards.push(firstCard);
+
+        for (const genSuit in Suit) {
+            if (genSuit.length > 2) {
+                const firstCard = new Card(suitLookup(genSuit), {
+                    faceValue: FaceValue.Ace,
+                    pointValue: 1
+                });
+                deck.cards.push(firstCard);
+            }
+        }
         return deck;
     }
 }
+// class method or function
+// export namespace method on the enum
+function suitLookup(suitLookup: string): Suit {
+    switch (suitLookup.toLowerCase()) {
+        case 'club':
+            return Suit.Club;
+        case 'diamond':
+            return Suit.Diamond;
+        case 'heart':
+            return Suit.Heart;
+        default:
+            return Suit.Spade;
+    }
+}
+
